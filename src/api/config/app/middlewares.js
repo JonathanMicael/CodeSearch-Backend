514011a25f511df79347c231592d5d9fa06f2609
Jonathan Micael <jonathanmicaell917@gmail.com>
2020-03-13 11:26:19 -0300

    resolvendo imports

--- src/api/config/app/middlewares.js
+++ src/api/config/app/middlewares.js
@@ -24,7 +24,6 @@ module.exports = app => {
   );
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(passport.initialize());
   app.use(multer({ storage }).array("arquivos"));
 
   return app;
