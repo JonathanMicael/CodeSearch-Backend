df434dfb77706f52948dca6ed62e26677234089d
Jonathan Micael <jonathanmicaell917@gmail.com>
2020-03-13 11:13:09 -0300

    resolvendo cors

--- src/api/config/app/middlewares.js
+++ src/api/config/app/middlewares.js
@@ -1,17 +1,16 @@
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport");
-const multer = require('multer');
-const uuid = require('uuid/v4')
-const path = require('path')
+const multer = require("multer");
+const uuid = require("uuid/v4");
+const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: path.resolve(__dirname, '..', '..','..', 'uploads'),
+  destination: path.resolve(__dirname, "..", "..", "..", "uploads"),
   filename: (req, file, cb, filename) => {
-      cb(null, uuid() + path.extname(file.originalname));
+    cb(null, uuid() + path.extname(file.originalname));
   }
 });
-
 
 /**
  * Módulo para inserir os middlewares no app express.
@@ -19,11 +18,15 @@ const storage = multer.diskStorage({
  * @returns {express} app com middlewares injetados.
  */
 module.exports = app => {
-  app.use(cors());
+  app.use(
+    cors({
+      origin: ["*"]
+    })
+  );
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(passport.initialize());
-  app.use(multer({storage}).array('arquivos'));
+  app.use(multer({ storage }).array("arquivos"));
 
   return app;
 };
